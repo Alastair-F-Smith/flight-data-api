5f0ea6fb9c7026ef4ad461613989c4253c3fda29
Alastair Smith <alithton@proton.me>
2024-06-11 13:06:36 +0100

    Add role-based authorisation to mutating end points so that only admins can access them

--- src/test/java/com/example/afs/flightdataapi/controllers/AircraftDataControllerTests.java
+++ src/test/java/com/example/afs/flightdataapi/controllers/AircraftDataControllerTests.java
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -100,7 +101,7 @@ class AircraftDataControllerTests {
                 );
     }
 
-    @WithMockUser
+    @WithMockUser(roles = "ADMIN")
     @Test
     @DisplayName("Add aircraft passes correct aircraft data to the repository")
     void addAircraftPassesCorrectAircraftDataToTheRepository() throws Exception {
@@ -121,7 +122,7 @@ class AircraftDataControllerTests {
         verify(aircraftsDataRepository, times(1)).save(aircraft1);
     }
 
-    @WithMockUser
+    @WithMockUser(roles = "ADMIN")
     @Test
     @DisplayName("Add aircraft returns the correct location URL for the newly created resource")
     void addAircraftReturnsTheCorrectLocationUrlForTheNewlyCreatedResource() throws Exception {
@@ -139,6 +140,21 @@ class AircraftDataControllerTests {
     }
 
     @WithMockUser
+    @Test
+    @DisplayName("Add aircraft returns a 403 response status when the user is not an admin")
+    void addAircraftReturnsA403ResponseStatusWhenTheUserIsNotAnAdmin() throws Exception {
+        String json = objectMapper.writeValueAsString(aircraft1);
+
+        when(aircraftsDataRepository.save(any(AircraftsData.class)))
+                .thenReturn(aircraft1);
+
+        mockMvc.perform(post("/api/aircraft")
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON))
+               .andExpect(status().isForbidden());
+    }
+
+    @WithMockUser(roles = "ADMIN")
     @Test
     @DisplayName("Update aircraft returns a response containing the updated data")
     void updateAircraftReturnsAResponseContainingTheUpdatedData() throws Exception {
@@ -161,7 +177,7 @@ class AircraftDataControllerTests {
                 );
     }
 
-    @WithMockUser
+    @WithMockUser(roles = "ADMIN")
     @Test
     @DisplayName("Update aircraft returns a not found status code if the ID is not found")
     void updateAircraftReturnsANotFoundStatusCodeIfTheIdIsNotFound() throws Exception {
@@ -181,6 +197,18 @@ class AircraftDataControllerTests {
 
     @WithMockUser
     @Test
+    @DisplayName("Update aircraft returns a 403 status code if user is not an admin")
+    void updateAircraftReturnsA403StatusCodeIfUserIsNotAnAdmin() throws Exception {
+        AircraftsData updated = new AircraftsData("ZZZ", new AircraftModel("Airbus", "Airbus"), 123456);
+
+        mockMvc.perform(put("/api/aircraft/" + aircraft1.getAircraftCode())
+                                .content(objectMapper.writeValueAsString(updated))
+                                .contentType(MediaType.APPLICATION_JSON))
+               .andExpect(status().isForbidden());
+    }
+
+    @WithMockUser(roles = "ADMIN")
+    @Test
     @DisplayName("Delete aircraft returns 200 status when aircraft record is found")
     void deleteAircraftReturns200StatusWhenAircraftRecordIsFound() throws Exception {
         when(aircraftsDataRepository.findById(anyString()))
@@ -190,7 +218,7 @@ class AircraftDataControllerTests {
                 .andExpect(status().isOk());
     }
 
-    @WithMockUser
+    @WithMockUser(roles = "ADMIN")
     @Test
     @DisplayName("Delete aircraft returns a response containing data for the deleted record")
     void deleteAircraftReturnsAResponseContainingDataForTheDeletedRecord() throws Exception {
@@ -205,7 +233,7 @@ class AircraftDataControllerTests {
                );
     }
 
-    @WithMockUser
+    @WithMockUser(roles = "ADMIN")
     @Test
     @DisplayName("Delete aircraft returns an error response when an aircraft record is not found")
     void deleteAircraftReturnsAnErrorResponseWhenAnAircraftRecordIsNotFound() throws Exception {
@@ -219,6 +247,17 @@ class AircraftDataControllerTests {
                        jsonPath("$.statusCode", is(404)),
                        jsonPath("$.cause", is(""))
                );
+    }
+
+    @WithMockUser
+    @Test
+    @DisplayName("Delete aircraft returns a 403 status code when user is not an admin")
+    void deleteAircraftReturnsA403StatusCodeWhenUserIsNotAnAdmin() throws Exception {
+        when(aircraftsDataRepository.findById(anyString()))
+                .thenReturn(Optional.of(aircraft1));
+
+        mockMvc.perform(delete("/api/aircraft/" + aircraft1.getAircraftCode()))
+               .andExpect(status().isForbidden());
     }
 
 }
