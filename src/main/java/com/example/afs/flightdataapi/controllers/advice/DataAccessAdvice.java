385df4820186a3162c03be8c833209dc1423f60c
Alastair Smith <alithton@proton.me>
2024-06-13 09:22:05 +0100

    Update controller advice to handle input validation exceptions

--- src/main/java/com/example/afs/flightdataapi/controllers/advice/DataAccessAdvice.java
+++ src/main/java/com/example/afs/flightdataapi/controllers/advice/DataAccessAdvice.java
@@ -1,16 +1,26 @@
 package com.example.afs.flightdataapi.controllers.advice;
 
+import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.NestedRuntimeException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class DataAccessAdvice {
 
     @ExceptionHandler({DataAccessException.class, TransactionException.class})
@@ -25,5 +35,19 @@ public class DataAccessAdvice {
         ErrorResponse response = ErrorResponse.from(e, HttpStatus.NOT_FOUND);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.from(e, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest()
+                             .body(response);
+    }
+
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<ErrorResponse> handleCustomValidationException(CustomValidationException e) {
+        ErrorResponse response = ErrorResponse.from(e, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest()
+                             .body(response);
     }
 }
--- src/main/java/com/example/afs/flightdataapi/controllers/advice/ErrorResponse.java
+++ src/main/java/com/example/afs/flightdataapi/controllers/advice/ErrorResponse.java
@@ -1,13 +1,49 @@
 package com.example.afs.flightdataapi.controllers.advice;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.NestedRuntimeException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.stream.Collectors;
 
 public record ErrorResponse(String message, int statusCode, String cause) {
 
     public static ErrorResponse from(Exception e, HttpStatusCode statusCode) {
         String cause = getRootCause(e);
+        if (cause.isEmpty()) cause = e.getClass().getName();
         return new ErrorResponse(e.getMessage(), statusCode.value(), cause);
+    }
+
+    public static ErrorResponse from(CustomValidationException e, HttpStatusCode statusCode) {
+        String cause = e.getErrors()
+                        .getAllErrors()
+                        .stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.joining("; "));
+        return new ErrorResponse(e.getMessage(), statusCode.value(), cause);
+    }
+
+    public static ErrorResponse from(MethodArgumentNotValidException e, HttpStatusCode statusCode) {
+        String cause = formatFieldErrorDefaultMessages(e);
+        String invalidFields = formatInvalidFields(e);
+        return new ErrorResponse("Error when validating input data for fields:" + invalidFields, statusCode.value(), cause);
+    }
+
+    private static String formatFieldErrorDefaultMessages(MethodArgumentNotValidException e) {
+        return e.getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+    }
+
+    private static String formatInvalidFields(MethodArgumentNotValidException e) {
+        return e.getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": rejected value " +
+                        fieldError.getRejectedValue())
+                .collect(Collectors.joining(", ", " [", "]"));
     }
 
     private static String getRootCause(Exception e) {
