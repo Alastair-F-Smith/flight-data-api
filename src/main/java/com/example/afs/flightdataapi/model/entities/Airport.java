79e1becefa6d32db199a592180c578795d321bf1
Alastair Smith <alithton@proton.me>
2024-06-14 10:30:53 +0100

    Add support for airport data access

---
+++ src/main/java/com/example/afs/flightdataapi/model/entities/Airport.java
@@ -0,0 +1,117 @@
+package com.example.afs.flightdataapi.model.entities;
+
+import com.example.afs.flightdataapi.services.converters.PGPointType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
+import org.hibernate.validator.constraints.Length;
+import org.locationtech.jts.geom.Point;
+import org.postgresql.geometric.PGpoint;
+import java.util.TimeZone;
+
+@Entity
+@Table(name = "airports_data")
+public class Airport {
+
+    public static final String AIRPORT_CODE_LENGTH_MESSAGE = "Airport code must be exactly 3 characters long";
+    public static final String AIRPORT_CODE_NULL_MESSAGE = "An airport code must be provided";
+
+    @NotBlank(message = AIRPORT_CODE_NULL_MESSAGE)
+    @Length(min = 3, max = 3, message = AIRPORT_CODE_LENGTH_MESSAGE)
+    @Id
+    String airportCode;
+
+    @NotNull
+    @JdbcTypeCode(SqlTypes.JSON)
+    TranslatedField airportName;
+
+    @NotNull
+    @JdbcTypeCode(SqlTypes.JSON)
+    TranslatedField city;
+
+    @NotNull
+    @Type(PGPointType.class)
+    PGpoint coordinates;
+
+    @NotNull
+    TimeZone timezone;
+
+    public Airport(String airportCode, TranslatedField airportName, TranslatedField city, PGpoint coordinates, TimeZone timezone) {
+        this.airportCode = airportCode;
+        this.airportName = airportName;
+        this.city = city;
+        this.coordinates = coordinates;
+        this.timezone = timezone;
+    }
+
+    public Airport() {
+    }
+
+    public @NotBlank(message = AIRPORT_CODE_NULL_MESSAGE) @Length(min = 3, max = 3, message = AIRPORT_CODE_LENGTH_MESSAGE) String getAirportCode() {
+        return airportCode;
+    }
+
+    public void setAirportCode(@NotBlank(message = AIRPORT_CODE_NULL_MESSAGE) @Length(min = 3, max = 3, message = AIRPORT_CODE_LENGTH_MESSAGE) String airportCode) {
+        this.airportCode = airportCode;
+    }
+
+    public @NotNull TranslatedField getAirportName() {
+        return airportName;
+    }
+
+    public void setAirportName(@NotNull TranslatedField airportName) {
+        this.airportName = airportName;
+    }
+
+    public @NotNull TranslatedField getCity() {
+        return city;
+    }
+
+    public void setCity(@NotNull TranslatedField city) {
+        this.city = city;
+    }
+
+    public @NotNull PGpoint getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(@NotNull PGpoint coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public @NotNull TimeZone getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(@NotNull TimeZone timezone) {
+        this.timezone = timezone;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "airportCode='" + airportCode + '\'' +
+                ", airportName=" + airportName +
+                ", city=" + city +
+                ", coordinates='" + coordinates + '\'' +
+                ", timezone=" + timezone +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Airport airport = (Airport) o;
+        return airportCode.equals(airport.airportCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return airportCode.hashCode();
+    }
+}
--- src/test/resources/scripts/init-db.sql
+++ src/test/resources/scripts/init-db.sql
@@ -6,8 +6,16 @@ create table if not exists aircrafts_data
 );
 
 CREATE TABLE IF NOT EXISTS seats (
-                       aircraft_code character(3) NOT NULL,
-                       seat_no character varying(4) NOT NULL,
-                       fare_conditions character varying(10) NOT NULL,
-                       CONSTRAINT seats_fare_conditions_check CHECK (((fare_conditions)::text = ANY (ARRAY[('Economy'::character varying)::text, ('Comfort'::character varying)::text, ('Business'::character varying)::text])))
+   aircraft_code character(3) NOT NULL,
+   seat_no character varying(4) NOT NULL,
+   fare_conditions character varying(10) NOT NULL,
+   CONSTRAINT seats_fare_conditions_check CHECK (((fare_conditions)::text = ANY (ARRAY[('Economy'::character varying)::text, ('Comfort'::character varying)::text, ('Business'::character varying)::text])))
+);
+
+CREATE TABLE IF NOT EXISTS airports_data (
+   airport_code character(3) NOT NULL,
+   airport_name jsonb NOT NULL,
+   city jsonb NOT NULL,
+   coordinates point NOT NULL,
+   timezone text NOT NULL
 );
